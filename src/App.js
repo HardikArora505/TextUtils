089055d9220f16c338c31cc7c14b98d59b63906d
HardikArora505 <hardikarora1611@gmail.com>
2022-08-16 22:28:32 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-
+import { useState } from 'react';
 import './App.css';
-import About from './Components/About';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
-// import TextForm from './Components/TextForm';
+import TextForm from './Components/TextForm';
 
 
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='rgb(0 0 0 / 94%)'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils2" aboutText="aboutUs"/>
+      <Navbar title="TextUtils2" aboutText="aboutUs" mode={mode} toggleMode={toggleMode}/>
       {/* <Navbar /> */}
       <div className="container">
-        {/* <TextForm heading="Enter text to analyze"/> */}
-        <About/>
+        <TextForm heading="Enter text to analyze" mode={mode}/>
+        {/* <About/> */}
 
       </div>
     </>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -21,22 +21,22 @@ function TextForm(props) {
     }
     return (
         <>
-            <div>
+            <div style={{color:props.mode==='dark'?'white':'black'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" id="myBox" onChange={handleChange} value={text} rows="8"></textarea>
+                    <textarea className="form-control" id="myBox" onChange={handleChange} style={{backgroundColor:props.mode==='dark'?'#282b33':'white' , color:props.mode==='dark'?'white':'black'}} value={text} rows="8"></textarea>
                     <button className="btn btn-primary my-2" onClick={handleClick} >to upperCase</button>
                     <button className="btn btn-primary my-2 mx-2" onClick={handleLowClick} >to LowerCase</button>
                     <button className="btn btn-primary my-2 mx-2" onClick={handleClear} >Clear</button>
                 </div>
 
             </div>
-            <div className="container my-3" >
+            <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
                 <h1>Summary</h1>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
                 <p>{(1/125)*text.split(" ").length} Minutes required to read</p>
                 <h3>Preview</h3>
-                <p>{text}</p>
+                <p>{text.length>0?text:"Enter something in textbox to preview here"}</p>
 
             </div>
         </>
